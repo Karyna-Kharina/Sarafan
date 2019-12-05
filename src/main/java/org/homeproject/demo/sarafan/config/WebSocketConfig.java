b3f26ebe3946cb73b00028215cb994e57b3f46f0
karinka <kharinakaryna@gmail.com>
2019-12-05 22:49:17 +0200

    fixed

--- src/main/java/org/homeproject/demo/sarafan/config/WebSocketConfig.java
+++ src/main/java/org/homeproject/demo/sarafan/config/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/hs-guide-websocket").withSockJS();
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
     }
 }
--- src/main/java/org/homeproject/demo/sarafan/controller/MainController.java
+++ src/main/java/org/homeproject/demo/sarafan/controller/MainController.java
@@ -3,6 +3,7 @@ package org.homeproject.demo.sarafan.controller;
 import org.homeproject.demo.sarafan.domain.User;
 import org.homeproject.demo.sarafan.repository.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +17,8 @@ import java.util.HashMap;
 public class MainController {
 
     private final MessageRepo messageRepo;
+    @Value("${spring.profiles.active}")
+    private String profile;
 
     @Autowired
     public MainController(MessageRepo messageRepo) {
@@ -31,6 +34,8 @@ public class MainController {
         data.put("messages", messageRepo.findAll());
 
         model.addAttribute("frontendData", data);
+        model.addAttribute("isDevMode", "dev".equals(profile));
+
         return "index";
     }
 }
